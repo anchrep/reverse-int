01fb02a4b03ec253c456ba31d9e3cabcaf9a3257
anchrep <ach4bsns@gmail.com>
2021-08-21 19:51:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+ // Берем модуль, чтобы сразу избавиться от минуса
+ // конвертируем цифры в строку, чтобы работал split
+ // spilt делает из строки массив, чтобы работал reverse
+ // reverse 
+ // Join объединяет все элементы массива в строку.
+    let nToAbs = Math.abs(n);
+    return nToAbs.toString().split('').reverse().join('');
 }
